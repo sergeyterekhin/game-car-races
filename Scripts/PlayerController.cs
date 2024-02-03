4921df8545597a51bf3191a316eb1a19416ba387
sergeyterekhin <chupakabra228228@gmail.com>
2024-02-03 15:03:55 +0700

    add cinemachine

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
         var rb = this.GetComponent<Rigidbody2D>();
+        if (rb.velocity.x >= 10f) movement.x = 0f; 
         if (Vector2.zero != movement) rb.AddForce (movement *speed*10f);
  
     }
