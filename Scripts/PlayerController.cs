450923c5773c2a38aebd7dc592871e7e25d36080
sergeyterekhin <chupakabra228228@gmail.com>
2024-02-06 08:03:09 +0700

    cinemachine tuned

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float minVelocity;
+    private Rigidbody2D rb;
+    private bool canDriveBack;
+
+    public bool CanDriveBack 
+    {
+        get {
+            return canDriveBack;
+        }
+        set {
+            rb.velocity = new Vector2(0, 0);
+            canDriveBack = value;
+        }
+    }
+
     void Start()
     {
-        
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.canDriveBack = true;
     }
 
     void FixedUpdate()
     {
         Vector2 movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
-        var rb = this.GetComponent<Rigidbody2D>();
-        if (rb.velocity.x >= 10f) movement.x = 0f; 
-        if (Vector2.zero != movement) rb.AddForce (movement *speed*10f);
- 
+        if (Vector2.zero != movement) this.ChangeSpeedPlayer(movement);
+    }
+   
+
+    private void ChangeSpeedPlayer(Vector2 movement)
+    {
+        if (rb.velocity.x > 0f && movement.x < 0f) movement.x = movement.x - 2f;
+        else if (rb.velocity.x < 0f && movement.x > 0f) movement.x = movement.x + 2f;
+
+        if ((rb.velocity.x >= this.maxVelocity && movement.x >= 0f)
+            || (rb.velocity.x <= this.minVelocity && movement.x <= 0f)
+            || (movement.x < 0 && !canDriveBack))
+        { 
+            movement.x = 0f; 
+        }
+        
+        rb.AddForce(new Vector2(movement.x * speed * 10f, movement.y * speed * 10f));
     }
 }
--- Scripts/ScopeOfVisibility.cs
+++ Scripts/ScopeOfVisibility.cs
@@ -7,21 +7,29 @@ using System.Linq;
 public class ScopeOfVisibility : MonoBehaviour
 {
     private string[] relocatedObject = new string[] { "Road", "Background", "el1bg", "el2bg" };
+    [SerializeField] private GameObject mainCamera;
     void OnTriggerExit2D(Collider2D collision)
     {
         if (relocatedObject.Contains(collision.tag)) ElementCreator.Relocate(collision.gameObject, collision.tag);
-        //switch (collision.tag)
-        //{
-        //    case "Road":
-        //        ElementCreator.Relocate(collision.gameObject, "roads");
-        //    break;
-        //    case "Background":
-        //        ElementCreator.Relocate(collision.gameObject, "backgrounds");
-        //    break;
-        //    case "el1bg":
-        //        ElementCreator.Relocate(collision.gameObject, "elementsBackground");
-        //    break;
-        //    default: break;
-        //}
+        else if (collision.tag == "PlayerBound")
+        {
+            var mainPlayer = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
+            mainPlayer.CanDriveBack = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerBound")
+        {
+            var mainPlayer = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerController>();
+            if(!mainPlayer.CanDriveBack) mainPlayer.CanDriveBack=true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.transform.position.x + 0.2f < mainCamera.transform.position.x)
+        this.transform.position = new Vector3(mainCamera.transform.position.x-0.2f, this.transform.position.y, this.transform.position.z);
     }
 }
