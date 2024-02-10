97bf6db4dcb3861134ce228354b1be98756b2636
sergeyterekhin <chupakabra228228@gmail.com>
2024-02-10 23:32:45 +0700

    added the car stereo and its logic

---
+++ Scripts/AudioPlayer.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+[System.Serializable]
+public class AudioPlayer
+{
+    private List<AudioClip> audioFiles;
+    private int playedAudio=-1;
+     private string audioFolderPath;
+
+    [SerializeField] protected AudioSource BoomBox;
+
+    public string PlayedAudio
+    {
+        get
+        {
+            if (playedAudio == -1) return "OFF"; else return audioFiles[playedAudio].name;
+        }
+    }
+
+    public string[] AudioFiles
+    {
+        get
+        {
+            var list = new List<string>();
+            foreach(AudioClip audio in audioFiles)
+            {
+                list.Add(audio.name);
+            }
+            return list.ToArray();
+        }
+    }
+
+    public string AudioFolderPath
+    {
+        get
+        {
+            return this.audioFolderPath;
+        }
+
+        set
+        {
+            this.audioFolderPath = value;
+            this.audioFiles=this.InitAudioFolder();
+        }
+    }
+
+    public bool isPlaying()
+    {
+        return BoomBox.isPlaying;
+    }
+
+    public void PlayNext()
+    {
+        if (playedAudio + 1 == audioFiles.Count) playedAudio = 0;
+        else playedAudio++;
+        this.PlayByID(playedAudio);
+    }
+
+    public void PlayBack()
+    {
+        if (playedAudio - 1 < 0) playedAudio = audioFiles.Count-1;
+        else playedAudio--;
+        this.PlayByID(playedAudio);
+    }
+
+    public void PlayByID(int id)
+    {
+        if (audioFiles.Count > id)
+        {
+            BoomBox.Stop();
+            BoomBox.clip = audioFiles[id];
+            playedAudio = id;
+            BoomBox.Play();
+        }
+    }
+
+    public void PlayByName(string name)
+    {
+        int idAudio = audioFiles.FindIndex(x => x.name.ToLower() == name.ToLower());
+        if (idAudio != -1) this.PlayByID(idAudio);
+    }
+
+    private List<AudioClip> InitAudioFolder()
+    {
+        var audioFiles=new List<AudioClip>();
+        string[] AudoFilesPath = Directory.GetFiles("Assets/"+audioFolderPath, "*.mp3");
+        foreach(string pathToAudio in AudoFilesPath)
+        {
+            AudioClip audio = AssetDatabase.LoadAssetAtPath<AudioClip>(pathToAudio);
+            audioFiles.Add(audio);
+        }
+        return audioFiles;
+    }
+}
